847ee0f87df996f8f582afe89ea7309f1c4ddceb
bitan2501 <bitan.guha.2501@gmail.com>
2022-05-06 13:02:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "mybucket" {
   acl    = "public-read"
   # Add specefic S3 policy in the s3-policy.json on the same directory
   #policy = file("s3-policy.json")
-  policy = templatefile("templates/s3-policy.json", { bucket = "s3-mybucket-webapp.example.com" })
+  #policy = templatefile("templates/s3-policy.json", { bucket = "s3-mybucket-webapp.example.com" })
 
   versioning {
     enabled = false
